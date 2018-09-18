7fa239f87a6669fa22671b7fee105eeb3d825aea
Darruma <30578602+Darruma@users.noreply.github.com>
2018-09-18 10:50:01 +0100

    Added vote event

--- frontend/src/components/comment.jsx
+++ frontend/src/components/comment.jsx
@@ -4,17 +4,30 @@ import '../index.css'
 class Comment extends Component{
 
   state = {
-    id:this.props.id
+    points:this.props.points
     };
+    handleVote = (type,id) =>
+    {
+        fetch(this.props.type+"vote" ,{
+            method: "POST",
+            headers: {                        
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify({_id:id})
+        }).then(res => res.json()).then(res => {
+            console.log(res);
+            this.setState({points:this.state.points+1});
+        })
+    }
   render(){
     return (
       <div className="comment" id={this.props.id}>
       <div className="vote">
-        <Arrow type="up" status={this.props.vote}  key={this.props.id}/>
-        <Arrow type="down" status={this.props.vote}  key={this.props.id+1}/>
+        <Arrow type="up" status={this.props.vote} onClick={this.handleVote("up",this.props._id)} key={this.props.id}/>
+        <Arrow type="down" status={this.props.vote}  onClick={this.handleVote("down",this.props._id)} key={this.props.id+1}/>
        </div>
         <span className="author">{this.props.author} </span>
-        <span className="points">{this.props.points} points</span>
+        <span className="points">{this.state.points} points</span>
         <div className="content">{this.props.content}</div>
       </div>
     );
