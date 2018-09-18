8276014dd0b421b3c5b6d86c6381a0e570956266
Darruma <30578602+Darruma@users.noreply.github.com>
2018-09-18 10:56:26 +0100

    Removed unnecessary state

--- frontend/src/components/arrow.jsx
+++ frontend/src/components/arrow.jsx
@@ -2,25 +2,20 @@ import React, { Component } from 'react';
 import '../index.css'
 class Arrow extends Component {
 	state = { 
-		type:this.props.type,
 		status:this.props.status,
-		id:this.props.id 
 	}
 	render() { 
-		var arrow_className = "arrow " + this.state.type;
-		if(this.state.type == "up" && this.state.status ==1 )
+        let arrow_className = "arrow " + this.props.type;
+        
+		if(this.props.type == "up" && this.state.status == 1 )
 		{
 			arrow_className = "arrow upvoted"
 		}
-		 if(this.state.type == "down" && this.state.status == -1 )
+		 if(this.props.type == "down" && this.state.status == -1 )
 		{
 			arrow_className = "arrow downvoted"
 		}
 		return ( <div className={arrow_className}> </div> );
 	}
-componentDidMount()
-  {
-   // fetch('posts/score/' + this.state.id).then(res => res.json()).then(data => this.setState({data}));
-  }
-}
+ }
 export default Arrow;
