ca3a12fb864621a5e32c1bf4897fad364bdc3012
Darruma <30578602+Darruma@users.noreply.github.com>
2018-09-19 13:44:05 +0100

    added voting to arrows

--- frontend/src/components/arrow.jsx
+++ frontend/src/components/arrow.jsx
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import '../index.css'
+import React, { Component } from "react";
+import "../index.css";
 class Arrow extends Component {
-	state = { 
-		status:this.props.status,
-	}
-	render() { 
-        let arrow_className = "arrow " + this.props.type;
-        
-		if(this.props.type == "up" && this.state.status == 1 )
-		{
-			arrow_className = "arrow upvoted"
-		}
-		 if(this.props.type == "down" && this.state.status == -1 )
-		{
-			arrow_className = "arrow downvoted"
-		}
-		return ( <div className={arrow_className}> </div> );
-	}
- }
+  state = {
+    status: this.props.status
+  };
+  handleVote = e => {
+    e.preventDefault();
+    console.log("voting");
+    this.props.voteMethod(this.props.type, this.props.id);
+  };
+  render() {
+    let arrow_className = "arrow " + this.props.type;
+
+    if (this.props.type == "up" && this.state.status == 1) {
+      arrow_className = "arrow upvoted";
+    }
+    if (this.props.type == "down" && this.state.status == -1) {
+      arrow_className = "arrow downvoted";
+    }
+    return (
+      <div className={arrow_className} onClick={this.handleVote}>
+      </div>
+    );
+  }
+}
 export default Arrow;
