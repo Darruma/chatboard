32536a4ae908a251fb279fccae33cc5bbc350f38
Darruma <30578602+Darruma@users.noreply.github.com>
2018-09-19 13:45:43 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,70 @@
-import React, { Component } from 'react';
-import Comment from './components/comment'
-import './App.css';
+import React, { Component } from "react";
+import Comment from "./components/comment";
+import CommentBox from "./components/commentbox";
+import "./App.css";
 
 class App extends Component {
-
-  state = {data:[]};
+  state = { data: [], textvalue: "" };
   render() {
     console.log(this.state);
     return (
-      <div className="App" >
-      {this.state.data.map((element,i) =>
-      {
-        return (<div key={i}><Comment content={element.content} points={element.points} vote={element.vote} author={element.author} time ={element.timestamp} id={element.id} /><br/></div>)
-      })}
+      <div className="App">
+        {this.state.data.map((element, i) => {
+          return (
+            <div key={i}>
+              <Comment
+                content={element.content}
+                points={element.points}
+                vote={element.vote}
+                author={element.author}
+                time={element.timestamp}
+                id={element.id}
+              />
+              <br />
+            </div>
+          );
+        })}
+        <CommentBox
+          value={this.state.textvalue}
+          handleChange={this.handleCommentSubmit}
+          handleSubmit={this.handleCommentSubmit}
+        />
       </div>
     );
   }
-  componentDidMount()
-  {
+  componentDidMount() {
     // fetch('posts/homepage').then(res => res.json()).then(data => {
     //   console.log(data)
     // });
-    console.log("mounted");
-    const newState = [{content:"hello",points:5,vote:-1,author:"simon",time:"1 hour ago", id:54 },
-    {content:"world",points:6,vote:1,author:"bobby",time:"2 hours ago", id:222 }];
-    this.setState({data:newState});
+    const newState = [
+      {
+        content: "hello",
+        points: 5,
+        vote: -1,
+        author: "simon",
+        time: "1 hour ago",
+        id: 54
+      },
+      {
+        content: "world",
+        points: 6,
+        vote: 1,
+        author: "bobby",
+        time: "2 hours ago",
+        id: 222
+      }
+    ];
+    this.setState({ data: newState });
   }
+  handleCommentSubmit = e => {
+    event.preventDefault();
+  // Post comment data.
+    this.setState({textvalue: '' });
+  };
+
+  handleCommentChange = e => {
+    this.setState({ textvalue: e.target.value });
+  };
 }
 
 export default App;
