47ed8c61a6fa6654fe7e144d8ecc859a1b30812c
Darruma <30578602+Darruma@users.noreply.github.com>
2018-09-20 17:06:32 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,77 @@
-import React, { Component } from "react";
-import Comment from "./components/comment";
-import CommentBox from "./components/commentbox";
-import "./App.css";
+import React, { Component } from 'react';
+import Comment from './components/comment';
+import CommentBox from './components/commentbox';
+import './App.css';
 
 class App extends Component {
-  state = { data: [], textvalue: '' };
-  render() {
-    return (
-      <div className="App">
-        {this.state.data.map((element, i) => {
-          return (
-            <div key={i}>
-              <Comment
-                content={element.content}
-                points={element.points}
-                vote={element.vote}
-                author={element.author}
-                time={element.timestamp}
-                id={element.id}
-              />
-              <br />
-            </div>
-          );
-        })}
-        <CommentBox
-          value={this.state.textvalue}
-          handleChange={this.handleCommentChange}
-          handleSubmit={this.handleCommentSubmit}
-        />
-      </div>
-    );
-  }
-  componentDidMount() {
-    // fetch('posts/homepage').then(res => res.json()).then(data => {
-    //   console.log(data)
-    // });
-    const newState = [
-      {
-        content: "hello",
-        points: 5,
-        vote: -1,
-        author: "simon",
-        time: "1 hour ago",
-        id: 54
-      },
-      {
-        content: "world",
-        points: 6,
-        vote: 1,
-        author: "bobby",
-        time: "2 hours ago",
-        id: 222
-      }
-    ];
-    this.setState({ data: newState });
-  }
-  handleCommentSubmit = (e) => {
-    e.preventDefault();
-  // Post comment data.
-    this.setState({textvalue: '' });
-  };
-
-  handleCommentChange = (e) => {
-    this.setState({ textvalue: e.target.value });
-  };
+	state = { data: [], textvalue: '', commentsVisible: true};
+	render() {
+		const comments =
+			this.state.data.length && this.state.commentsVisible
+				? this.state.data.map((element, i) => {
+						return (
+							<div key={i}>
+								<Comment
+									content={element.content}
+									points={element.points}
+									vote={element.vote}
+									author={element.author}
+									time={element.timestamp}
+									id={element.id}
+								/>
+								<br />
+							</div>
+						);
+					}) 
+				: '';
+		return (
+			<div className="App">
+				<div className="comments">{comments}</div>
+				<CommentBox
+							value={this.state.textvalue}
+							handleChange={this.handleCommentChange}
+							handleSubmit={this.handleCommentSubmit}
+						/>
+			</div>
+		);
+	}
+	componentDidMount() {
+		fetch('posts/homepage').then((res) => res.json()).then((data) => {
+			console.log(data);
+			this.setState({ data: data.message });
+		});
+	}
+	handleCommentSubmit = (e) => {
+		e.preventDefault();	
+		post_data('/post', { content: this.state.textvalue }).then((res) => res.json()).then((res) => {
+			const newState = this.state;
+			if (res.success) {
+				const newComment = {
+					content: this.state.textvalue,
+					points: res.message.score,
+					vote: res.message.vote,
+					author: res.message.author,
+					time: res.message.timestamp,
+					id: res.message.id
+				};
+				newState.data.push(newComment);
+				newState.textvalue = '';
+				this.setState({ newState });
+			}
+		});
+	};
+	handleCommentChange = (e) => {
+		this.setState({ textvalue: e.target.value });
+	};
 }
-
+function post_data(url, data) {
+	return fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8'
+		},
+		body: JSON.stringify(data)
+	});
+}
 export default App;
+
